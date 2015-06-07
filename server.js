90e5ecb7ad2a70c536c1152c38a4101c3624728a
jrhall <jeffreyrhall@gmail.com>
2015-06-07 18:38:01 +0000

    Removed old, commented-out code.

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
 var app  = require('express')(); //instead of require('express') and then var app = express();
 var http = require('http').Server(app);//aka http.Server(app)?
 var io = require('socket.io')(http); //(http) same as io.listen(Server)
